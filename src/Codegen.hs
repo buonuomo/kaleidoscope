9c45df8ccd52dd6974d350040caff9519d72eb02
Noah Goodman <ngoodman@uchicago.edu>
2019-09-09 01:11:00 -0400

    Added unique name generation and block helpers

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Codegen where
 
@@ -6,8 +7,10 @@ import Control.Monad.State
 
 import           Data.Map (Map)
 import qualified Data.Map as Map
+import           Data.String
 
 import           LLVM.AST as AST
+import qualified LLVM.AST.Constant as C
 import           LLVM.AST.Global
 import           LLVM.AST.Linkage
 import qualified LLVM.Prelude as LP
@@ -17,13 +20,15 @@ double = FloatingPointType DoubleFP
 
 type SymbolTable = [(String, Operand)]
 
+type Names = Map String Int
+
 data CodegenState = CodegenState
   { currentBlock :: Name            -- name of active block to append to
   , blocks :: Map Name BlockState   -- Blocks for function
   , symtab :: SymbolTable           -- Function scope symbol table
   , blockCount :: Int               -- Count of basic blocks
   , count :: Word                   -- count of unnamed instructions
-  , names :: [Name]                 -- name supply
+  , names :: Names                  -- name supply
   } deriving (Show)
 
 data BlockState = BlockState
@@ -41,29 +46,106 @@ newtype LLVM a = LLVM (State AST.Module a)
 runLLVM :: AST.Module -> LLVM a -> AST.Module
 runLLVM mod (LLVM m) = execState m mod
 
-emptyModule :: LP.ShortByteString -> AST.Module
-emptyModule label = defaultModule { moduleName = label }
+emptyModule :: String -> AST.Module
+emptyModule label = defaultModule { moduleName = fromString label }
 
 addDefn :: Definition -> LLVM ()
 addDefn d = do
   defs <- gets moduleDefinitions
   modify $ \s -> s { moduleDefinitions = defs ++ [d] }
 
-definition :: Type -> LP.ShortByteString -> [(Type, Name)] -> [BasicBlock] -> LLVM ()
+definition :: Type -> String -> [(Type, Name)] -> [BasicBlock] -> LLVM ()
 definition retType label args body = addDefn $ 
   GlobalDefinition $ functionDefaults
     { returnType = retType
-    , name = Name label 
+    , name = mkName label 
     , parameters = (map (\(ty, nm) -> Parameter ty nm []) args, False)
     , basicBlocks = body
     }
 
-extern :: Type -> LP.ShortByteString -> [(Type, Name)] -> LLVM ()
+extern :: Type -> String -> [(Type, Name)] -> LLVM ()
 extern retType label args = addDefn $
   GlobalDefinition $ functionDefaults
     { linkage = External
     , returnType = retType
-    , name = Name label
+    , name = mkName label
     , parameters = (map (\(ty, nm) -> Parameter ty nm []) args, False)
     , basicBlocks = []
     }
+
+
+-- Block manipulation
+
+entry :: Codegen Name
+entry = gets currentBlock
+
+emptyBlock :: Int -> BlockState
+emptyBlock n = BlockState
+  { idx = n
+  , stack = []
+  , term = Nothing
+  }
+
+addBlock :: String -> Codegen Name
+addBlock bname = do
+  blks <- gets blocks
+  n <- gets blockCount
+  nms <- gets names
+  let new = emptyBlock n
+      (qname, supply) = uniqueName bname nms
+  modify $ \s ->
+    s { blocks = Map.insert (mkName qname) new blks
+      , blockCount = n + 1
+      , names = supply
+      }
+  return (mkName qname)
+
+setBlock :: Name -> Codegen Name
+setBlock bname = do
+  modify $ \s -> s { currentBlock = bname }
+  return bname
+
+getBlock :: Codegen Name
+getBlock = gets currentBlock
+
+modifyBlock :: BlockState -> Codegen ()
+modifyBlock new = do
+  curr <- getBlock
+  modify $ \s -> s { blocks = Map.insert curr new (blocks s) }
+
+current :: Codegen BlockState
+current = do
+  currentName <- getBlock
+  blks <- gets blocks
+  case Map.lookup currentName blks of
+    Nothing -> error $ "No such block: " ++ show currentName
+    Just bs -> return bs
+
+fresh :: Codegen Word
+fresh = do
+  idx <- gets count
+  modify $ \s -> s { count = idx + 1 }
+  return $ idx + 1
+
+uniqueName :: String -> Names -> (String, Names)
+uniqueName nm nms = 
+  case Map.lookup nm nms of
+    Nothing -> (nm, Map.insert nm 1 nms)
+    Just i -> (nm ++ show i, Map.insert nm (i + 1) nms)
+
+local :: Name -> Operand
+local = LocalReference double
+
+externf :: Name -> Operand
+externf = ConstantOperand . C.GlobalReference double
+
+assign :: String -> Operand -> Codegen ()
+assign var x = do
+  modify $ \s -> s { symtab = (var, x) : (symtab s) }
+
+getvar :: String -> Codegen Operand
+getvar var = do
+  locals <- gets symtab
+  case lookup var locals of
+    Nothing -> error $ "Local variable not in scope: " ++ var
+    Just op -> return op
